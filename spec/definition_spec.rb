ec0943eb024f53c0d320c95bf49b77625f9930ba
sr <skyrousse@gmail.com>
2016-07-08 11:48:51 -0700

    update variable names to include '_test' to make them more easily understood, aslo add spec for finding a definition by its id

--- spec/definition_spec.rb
+++ spec/definition_spec.rb
@@ -8,8 +8,8 @@ describe(Definition) do
 
     describe('#description') do
       it('returns the description of the object') do
-        cheapster = Definition.new('Someone who is cheap.')
-        expect(cheapster.description()).to(eq('Someone who is cheap.'))
+        cheapster_test_definition = Definition.new('Someone who is cheap.')
+        expect(cheapster_test_definition.description()).to(eq('Someone who is cheap.'))
       end
     end
 
@@ -21,15 +21,15 @@ describe(Definition) do
 
     describe('#save') do
       it('adds an object instance of Definition class to the definitions array') do
-        shypod = Definition.new('When one is hesitant about sharing the contents of his or her iPod.')
-        shypod.save()
-        expect(Definition.all()).to(eq([shypod]))
+        shypod_test_definition = Definition.new('When one is hesitant about sharing the contents of his or her iPod.')
+        shypod_test_definition.save()
+        expect(Definition.all()).to(eq([shypod_test_definition]))
       end
     end
 
     describe('.clear') do
       it('empties out all the saved definitions') do
-        cheapster = Definition.new('Someone who is cheap.').save()
+        cheapster_test_definition = Definition.new('Someone who is cheap.').save()
         Definition.clear()
         expect(Definition.all()).to(eq([]))
       end
@@ -37,19 +37,19 @@ describe(Definition) do
 
     describe('#id') do
       it('returns the id of the object') do
-        xzavier = Definition.new('person who is a complete badass and a great friend everyone wants to get to know him and hangout with him')
-        xzavier.save()
-        expect(xzavier.id()).to(eq(1))
+        xzavier_test_definition = Definition.new('A person who is a complete badass and a great friend everyone wants to get to know him and hangout with him')
+        xzavier_test_definition.save()
+        expect(xzavier_test_definition.id()).to(eq(1))
       end
     end
 
     describe('.find') do
       it('returns a definition by its id number') do
-        automagically = Definition.new("Something that happens automatically,  but in a way that, for some reason (typically because it is too complicated, or too ugly, or perhaps even too trivial), the speaker doesn't feel like explaining to you.")
-        automagically.save()
-        snoping = Definition.new("Verifying something you've seen on the internet on Snopes.com")
-        snoping.save()
-        expect(Definition.find(snoping.id())).to(eq(snoping))
+        automagically_test_definition = Definition.new("Something that happens automatically,  but in a way that, for some reason (typically because it is too complicated, or too ugly, or perhaps even too trivial), the speaker doesn't feel like explaining to you.")
+        automagically_test_definition.save()
+        snoping_test_definition = Definition.new("Verifying something you've seen on the internet on Snopes.com")
+        snoping_test_definition.save()
+        expect(Definition.find(snoping_test_definition.id())).to(eq(snoping_test_definition))
       end
     end
 
--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -9,16 +9,16 @@ describe(Word) do
 
   describe('#name') do
     it('returns the name of the word') do
-      cheapster_word = Word.new("Cheapster")
-      expect(cheapster_word.name()).to(eq("Cheapster"))
+      cheapster_test_word = Word.new("Cheapster")
+      expect(cheapster_test_word.name()).to(eq("Cheapster"))
     end
   end
 
   describe('#id') do
     it('returns the id of the word') do
-      kayleigh_word = Word.new("Kayleigh")
-      kayleigh_word.save()
-      expect(kayleigh_word.id()).to(eq(1))
+      kayleigh_test_word = Word.new("Kayleigh")
+      kayleigh_test_word.save()
+      expect(kayleigh_test_word.id()).to(eq(1))
     end
   end
 
@@ -30,16 +30,16 @@ describe(Word) do
 
   describe('#save') do
     it('saves a new instance of a Word object in the words array') do
-      shypod_word = Word.new('ShyPod')
-      shypod_word.save()
-      expect(Word.all()).to(eq([shypod_word]))
+      shypod_test_word = Word.new('ShyPod')
+      shypod_test_word.save()
+      expect(Word.all()).to(eq([shypod_test_word]))
     end
   end
 
   describe('.clear') do
     it('empties the words array') do
-      shypod_word = Word.new('Xzavier')
-      shypod_word.save()
+      shypod_test_word = Word.new('Xzavier')
+      shypod_test_word.save()
       Word.clear()
       expect(Word.all()).to(eq([]))
     end
@@ -47,18 +47,28 @@ describe(Word) do
 
   describe('#definitions') do
     it('initially returns an empty array of definitions objects in the word object') do
-      snoping_word = Word.new('Snoping')
-      snoping_word.save()
-      expect(snoping_word.definitions()).to(eq([]))
+      snoping_test_word = Word.new('Snoping')
+      snoping_test_word.save()
+      expect(snoping_test_word.definitions()).to(eq([]))
+    end
+  end
+
+  describe('.find') do
+    it('returns a word by its id number') do
+      xzavier_test_word = Word.new("Xzavier")
+      xzavier_test_word.save()
+      tinnies_test_word = Word.new("Tinnies")
+      tinnies_test_word.save()
+      expect(Word.find(xzavier_test_word.id())).to(eq(xzavier_test_word))
     end
   end
 
   # describe('#add_definition') do
   #   it('adds a new definition to a word') do
-  #     automagically_word = Word.new("Automagically")
+  #     automagically_test_word = Word.new("Automagically")
   #     automagically_definition = Definition.new("Something that happens automatically, but in a way that, for some reason (typically because it is too complicated, or too ugly, or perhaps even too trivial), the speaker doesn't feel like explaining to you.")
-  #     automagically_word.add_definition(automagically_definition)
-  #     expec(automagically_word()).to(eq([automagically_definition]))
+  #     automagically_test_word.add_definition(automagically_definition)
+  #     expec(automagically_test_word()).to(eq([automagically_definition]))
   #   end
   # end
 
